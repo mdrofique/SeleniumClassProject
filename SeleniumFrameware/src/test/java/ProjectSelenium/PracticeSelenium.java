43e8a834c06e6e08611988625549de905f70778e
Lenovo <Lenovo@DESKTOP-VQFTNGD.home>
2020-05-04 09:46:01 -0700

    first time send

---
+++ SeleniumFrameware/src/test/java/ProjectSelenium/PracticeSelenium.java
@@ -0,0 +1,38 @@
+package ProjectSelenium;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PracticeSelenium {
+	
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("http://newtours.demoaut.com");
+
+	    WebElement name=driver.findElement(By.xpath("//*[@type='text']"));
+	   name.sendKeys("rafique2009@yahoo.com");
+		
+		
+	   WebElement password=driver.findElement(By.xpath("//*[@type='password']"));
+		
+      password.sendKeys("r020281i");
+	
+		
+		
+		WebElement signin=driver.findElement(By.xpath("//*[@type='image']"));
+		signin.click();
+			
+			
+		
+	}
+
+}
